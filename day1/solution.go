816a0a60ef8cc8c876c9eafbe1f44e9abe4ac94c
Andre Pacheco <andrepacheco@Andres-MacBook-Pro.local>
2018-12-08 16:49:11 -0700

    Finished the first day's problem set.

---
+++ day1/solution.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// Streamline error handling
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	// Read the input file data
+	data, err := os.Open("input.txt")
+	check(err)
+
+	// We need a variable to keep track of the total
+	total := 0
+
+	// We need a map to keep track of the total counts
+	totalMap := make(map[int]int, 0)
+	totalMap[total]++
+
+	// Read the data line-by-line with a scanner
+	// Each line is of the format '+#' or '-#'
+	solutionFound := false
+	fileScanner := bufio.NewScanner(data)
+	allNumbers := make([]int, 0)
+	for fileScanner.Scan() {
+
+		// We need to convert each line into a string
+		numString := fileScanner.Text()
+
+		// Get the number (the sign is parsed by strconv.Atoi)
+		num, err := strconv.Atoi(numString)
+		check(err)
+
+		// Add the number to the list
+		allNumbers = append(allNumbers, num)
+
+	}
+
+	// We need to loop over after creating our total and search for a duplicate total value added to our map
+	for !solutionFound {
+
+		// Increment/Decrement the total and update the count in the map for each number
+		for _, num := range allNumbers {
+			total += num
+			totalMap[total]++
+
+			// Check for a duplicate total
+			if totalMap[total] == 2 {
+				fmt.Print("\nTotal Count: ", total, "\n\n")
+				solutionFound = true
+				break
+			}
+
+		}
+
+	}
+
+	// Output the answer
+	fmt.Print("\nTotal Sum: ", total, "\n\n")
+
+}
