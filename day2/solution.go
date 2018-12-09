84ceeb25d0413901b6e40c68c71d95b6b6abfc1b
Andre Pacheco <andrepacheco@Andres-MacBook-Pro.local>
2018-12-08 20:35:32 -0700

    Solved the second part of day 2.

--- day2/solution.go
+++ day2/solution.go
@@ -41,24 +41,55 @@ func main() {
 	twoSum := 0
 	threeSum := 0
 
-	// Go through each line to find matching runes
-	for _, line := range allLines {
-		checkSum(line, &twoSum, &threeSum)
+	// Go through each line to find matching runes and boxes
+	correctBoxes := make([]string, 0)
+	for _, line1 := range allLines {
+		checkSum(line1, &twoSum, &threeSum)
+		for _, line2 := range allLines {
+
+			// We don't want to add the match of a line to itself
+			if line1 == line2 {
+				continue
+			}
+
+			// If the count of matching letters is 1 - len(line), add both to the correct box list
+			matchCount := 0
+			for i, _ := range line1 {
+
+				// Count matching letters
+				if line1[i] == line2[i] {
+					matchCount++
+				}
+
+				// Add the boxes if we've found a ~match
+				if matchCount == (len(line1) - 1) {
+					correctBoxes = append(correctBoxes, line1)
+					correctBoxes = append(correctBoxes, line2)
+				}
+
+			}
+
+		}
+	}
+
+	// Get the letters in common between the correct boxes
+	box1 := correctBoxes[0]
+	box2 := correctBoxes[1]
+	commonLetters := ""
+	for i := range box1 {
+		if box1[i] == box2[i] {
+			commonLetters += string(box1[i])
+		}
 	}
 
 	// Generate the check sum
 	checkSum := twoSum * threeSum
-	fmt.Println(twoSum, threeSum)
 
 	// Output the answer
 	end := time.Now()
-	fmt.Print(
-		"\n",
-		"Check Sum: ", checkSum,
-		"\n",
-		"Time Taken: ", end.Sub(start),
-		"\n\n",
-	)
+	fmt.Println("Check Sum: ", checkSum)
+	fmt.Println("Box Letters: ", commonLetters)
+	fmt.Println("Time Taken: ", end.Sub(start))
 
 }
 
